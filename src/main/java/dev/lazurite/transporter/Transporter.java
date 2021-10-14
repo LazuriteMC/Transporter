33673674fbecf870606e2221106832cfb9b4cd17
tmvkrpxl0 <ironman466@naver.com>
2021-10-15 08:39:12 +0900

    Fixed event/packet registration

--- src/main/java/dev/lazurite/transporter/Transporter.java
+++ src/main/java/dev/lazurite/transporter/Transporter.java
@@ -2,11 +2,16 @@ package dev.lazurite.transporter;
 
 import dev.lazurite.transporter.api.buffer.PatternBuffer;
 import dev.lazurite.transporter.impl.buffer.PatternBufferImpl;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.Map;
 
 /**
  * The client and server entrypoint for Transporter. It also
@@ -21,11 +26,23 @@ public class Transporter {
 
     private static final PatternBuffer patternBuffer = new PatternBufferImpl();
 
+    public Transporter(){
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
 
-    @SubscribeEvent
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
     public void setup(final FMLCommonSetupEvent event) {
         LOGGER.info("Highly illogical.");
         TransporterPacketHandler.registerPackets();
+        ModList.get().getMods().forEach(modInfo -> {
+            if(modInfo.getModId().equals(MODID)){
+                Map<String, Object> properties = modInfo.getModProperties();
+                properties.keySet().forEach(key -> {
+                    LOGGER.info("key: {} value: {}", key, properties.get(key));
+                });
+            }
+        });
     }
 
     public static PatternBuffer getPatternBuffer() {
